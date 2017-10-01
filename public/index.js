75d28c83400726d9df0edb74670607baf51f1652
Kai Hsia <kaihsia@users.noreply.github.com>
2017-10-01 08:50:25 -0700

    bug fix 1

--- public/index.js
+++ public/index.js
@@ -97,22 +97,402 @@ var data = [
         .delay(delay);
   }
 
-});
+  // tree line
 
-var width = 500;
-var rectAndCircleTestData = [
-    {times: [{"starting_time": 1355752800000,
-             "display": "circle"}, {"starting_time": 1355767900000, "ending_time": 1355774400000}]},
-    {times: [{"starting_time": 1355759910000,
-    "display":"circle"}, ]},
-    {times: [{"starting_time": 1355761910000, "ending_time": 1355763910000}]}
-  ];
 
-function timelineRectAndCircle() {
-    var chart = d3.timeline();
 
-    var svg = d3.select("#timeline2_combine").append("svg").attr("width", width)
-      .datum(rectAndCircleTestData).call(chart);
+
+  var margin = {top: 20, right: 120, bottom: 20, left: 120},
+      width = 960 - margin.right - margin.left,
+      height = 800 - margin.top - margin.bottom;
+
+  var i = 0,
+      duration = 750,
+      root;
+
+  var tree = d3.layout.tree()
+      .size([height, width]);
+
+  var diagonal = d3.svg.diagonal()
+      .projection(function(d) { return [d.y, d.x]; });
+
+  var svg = d3.select("body").append("svg")
+      .attr("width", width + margin.right + margin.left)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  d3.json("twitter.json", function(error, flare) {
+    if (error) throw error;
+    console.log(flare);
+    root = flare;
+    root.x0 = height / 2;
+    root.y0 = 0;
+
+    function collapse(d) {
+      if (d.children) {
+        d._children = d.children;
+        d._children.forEach(collapse);
+        d.children = null;
+      }
+    }
+
+    root.children.forEach(collapse);
+    update(root);
+  });
+
+  d3.select(self.frameElement).style("height", "800px");
+
+  function update(source) {
+
+    // Compute the new tree layout.
+    var nodes = tree.nodes(root).reverse(),
+        links = tree.links(nodes);
+
+    // Normalize for fixed-depth.
+    nodes.forEach(function(d) { d.y = d.depth * 180; });
+
+    // Update the nodes…
+    var node = svg.selectAll("g.node")
+        .data(nodes, function(d) { return d.id || (d.id = ++i); });
+
+    // Enter any new nodes at the parent's previous position.
+    var nodeEnter = node.enter().append("g")
+        .attr("class", "node")
+        .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
+        .on("click", click);
+
+    nodeEnter.append("circle")
+        .attr("r", 1e-6)
+        .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+
+    nodeEnter.append("text")
+        .attr("x", function(d) { return d.children || d._children ? -10 : 10; })
+        .attr("dy", ".35em")
+        .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
+        .text(function(d) { return d.name; })
+        .style("fill-opacity", 1e-6);
+
+    // Transition nodes to their new position.
+    var nodeUpdate = node.transition()
+        .duration(duration)
+        .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
+
+    nodeUpdate.select("circle")
+        .attr("r", 4.5)
+        .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+
+    nodeUpdate.select("text")
+        .style("fill-opacity", 1);
+
+    // Transition exiting nodes to the parent's new position.
+    var nodeExit = node.exit().transition()
+        .duration(duration)
+        .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
+        .remove();
+
+    nodeExit.select("circle")
+        .attr("r", 1e-6);
+
+    nodeExit.select("text")
+        .style("fill-opacity", 1e-6);
+
+    // Update the links…
+    var link = svg.selectAll("path.link")
+        .data(links, function(d) { return d.target.id; });
+
+    // Enter any new links at the parent's previous position.
+    link.enter().insert("path", "g")
+        .attr("class", "link")
+        .attr("d", function(d) {
+          var o = {x: source.x0, y: source.y0};
+          return diagonal({source: o, target: o});
+        });
+
+    // Transition links to their new position.
+    link.transition()
+        .duration(duration)
+        .attr("d", diagonal);
+
+    // Transition exiting nodes to the parent's new position.
+    link.exit().transition()
+        .duration(duration)
+        .attr("d", function(d) {
+          var o = {x: source.x, y: source.y};
+          return diagonal({source: o, target: o});
+        })
+        .remove();
+
+    // Stash the old positions for transition.
+    nodes.forEach(function(d) {
+      d.x0 = d.x;
+      d.y0 = d.y;
+    });
+
+
+    var testData = [
+        {times: [{"starting_time": 1355752800000, "ending_time": 1355759900000}, {"starting_time": 1355767900000, "ending_time": 1355774400000}]},
+        {times: [{"starting_time": 1355759910000, "ending_time": 1355761900000}, ]},
+        {times: [{"starting_time": 1355761910000, "ending_time": 1355763910000}]}
+      ];
+
+      var rectAndCircleTestData = [
+        {times: [{"starting_time": 1355752800000,
+                 "display": "circle"}, {"starting_time": 1355767900000, "ending_time": 1355774400000}]},
+        {times: [{"starting_time": 1355759910000,
+        "display":"circle"}, ]},
+        {times: [{"starting_time": 1355761910000, "ending_time": 1355763910000}]}
+      ];
+
+      var labelTestData = [
+        {label: "person a", times: [{"starting_time": 1355752800000, "ending_time": 1355759900000}, {"starting_time": 1355767900000, "ending_time": 1355774400000}]},
+        {label: "person b", times: [{"starting_time": 1355759910000, "ending_time": 1355761900000}, ]},
+        {label: "person c", times: [{"starting_time": 1355761910000, "ending_time": 1355763910000}]}
+      ];
+
+      var iconTestData = [
+        {class:"jackie", icon: "jackie.png", times: [
+          {"starting_time": 1355752800000, "ending_time": 1355759900000}, 
+          {"starting_time": 1355767900000, "ending_time": 1355774400000}]},
+        {class:"troll", icon: "troll.png", times: [
+          {"starting_time": 1355759910000, "ending_time": 1355761900000,
+          "display":"circle"}, ]},
+        {class:"wat", icon: "wat.png", times: [
+          {"starting_time": 1355761910000, "ending_time": 1355763910000}]}
+      ];
+
+      var labelColorTestData = [
+        {label: "person a", times: [{"color":"green", "label":"Weeee", "starting_time": 1355752800000, "ending_time": 1355759900000}, {"color":"blue", "label":"Weeee", "starting_time": 1355767900000, "ending_time": 1355774400000}]},
+        {label: "person b", times: [{"color":"pink", "label":"Weeee", "starting_time": 1355759910000, "ending_time": 1355761900000}, ]},
+        {label: "person c", times: [{"color":"yellow", "label":"Weeee", "starting_time": 1355761910000, "ending_time": 1355763910000}]}
+      ];
+
+      var testDataWithColor = [
+        {label: "fruit 1", fruit: "orange", times: [
+          {"starting_time": 1355759910000, "ending_time": 1355761900000}]},
+        {label: "fruit 2", fruit: "apple", times: [
+          {"starting_time": 1355752800000, "ending_time": 1355759900000},
+          {"starting_time": 1355767900000, "ending_time": 1355774400000}]},
+        {label: "fruit3", fruit: "lemon", times: [
+          {"starting_time": 1355761910000, "ending_time": 1355763910000}]}
+        ];
+
+      var testDataWithColorPerTime = [
+        {label: "fruit 2", fruit: "apple", times: [
+          {fruit: "orange", "starting_time": 1355752800000, "ending_time": 1355759900000},
+          {"starting_time": 1355767900000, "ending_time": 1355774400000},
+          {fruit: "lemon", "starting_time": 1355774400000, "ending_time": 1355775500000}]}
+        ];
+
+      var testDataRelative = [
+        {times: [{"starting_time": 1355752800000, "ending_time": 1355759900000}, {"starting_time": 1355767900000, "ending_time": 1355774400000}]},
+        {times: [{"starting_time": 1355759910000, "ending_time": 1355761900000}]},
+        {times: [{"starting_time": 1355761910000, "ending_time": 1355763910000}]}
+      ];
+
+      var width = 500;
+      function timelineRect() {
+        var chart = d3.timeline();
+
+        var svg = d3.select("#timeline1").append("svg").attr("width", width)
+          .datum(testData).call(chart);
+      }
+      
+      function timelineRectNoAxis() {
+        var chart = d3.timeline().showTimeAxis();
+
+        var svg = d3.select("#timeline1_noaxis").append("svg").attr("width", width)
+          .datum(testData).call(chart);
+      }
+
+      function timelineCircle() {
+        var chart = d3.timeline()
+          .tickFormat( //
+            {format: d3.time.format("%I %p"),
+            tickTime: d3.time.hours,
+            tickInterval: 1,
+            tickSize: 30})
+          .display("circle"); // toggle between rectangles and circles
+
+        var svg = d3.select("#timeline2").append("svg").attr("width", width)
+          .datum(testData).call(chart);
+      }
+
+      function timelineRectAndCircle() {
+        var chart = d3.timeline();
+
+        var svg = d3.select("#timeline2_combine").append("svg").attr("width", width)
+          .datum(rectAndCircleTestData).call(chart);
+      }
+
+      function timelineHover() {
+        var chart = d3.timeline()
+          .width(width*4)
+          .stack()
+          .margin({left:70, right:30, top:0, bottom:0})
+          .hover(function (d, i, datum) {
+          // d is the current rendering object
+          // i is the index during d3 rendering
+          // datum is the id object
+            var div = $('#hoverRes');
+            var colors = chart.colors();
+            div.find('.coloredDiv').css('background-color', colors(i))
+            div.find('#name').text(datum.label);
+          })
+          .click(function (d, i, datum) {
+            alert(datum.label);
+          })
+          .scroll(function (x, scale) {
+            $("#scrolled_date").text(scale.invert(x) + " to " + scale.invert(x+width));
+          });
+
+        var svg = d3.select("#timeline3").append("svg").attr("width", width)
+          .datum(labelTestData).call(chart);
+      }
+
+      function timelineStackedIcons() {
+        var chart = d3.timeline()
+          .beginning(1355752800000) // we can optionally add beginning and ending times to speed up rendering a little
+          .ending(1355774400000)
+          .showTimeAxisTick() // toggles tick marks
+          .stack() // toggles graph stacking
+          .margin({left:70, right:30, top:0, bottom:0})
+          ;
+        var svg = d3.select("#timeline5").append("svg").attr("width", width)
+          .datum(iconTestData).call(chart);
+      }
+
+      function timelineLabelColor() {
+        var chart = d3.timeline()
+          .beginning(1355752800000) // we can optionally add beginning and ending times to speed up rendering a little
+          .ending(1355774400000)
+          .stack() // toggles graph stacking
+          .margin({left:70, right:30, top:0, bottom:0})
+          ;
+        var svg = d3.select("#timeline6").append("svg").attr("width", width)
+          .datum(labelColorTestData).call(chart);
+      }
+
+      function timelineRotatedTicks() {
+        var chart = d3.timeline()
+          .rotateTicks(45);
+
+        var svg = d3.select("#timeline7").append("svg").attr("width", width)
+          .datum(testData).call(chart);
+      }
+
+      function timelineRectColors() {
+
+        var colorScale = d3.scale.ordinal().range(['#6b0000','#ef9b0f','#ffee00'])
+            .domain(['apple','orange','lemon']);
+
+        var chart = d3.timeline()
+            .colors( colorScale )
+            .colorProperty('fruit');
+
+        var svg = d3.select("#timelineColors").append("svg").attr("width", width)
+          .datum(testDataWithColor).call(chart);
+      }
+
+      function timelineRectColorsPerTime() {
+        var colorScale = d3.scale.ordinal().range(['#6b0000','#ef9b0f','#ffee00'])
+          .domain(['apple','orange','lemon']);
+        var chart = d3.timeline()
+          .colors( colorScale )
+          .colorProperty('fruit');      
+        var svg = d3.select("#timelineColorsPerTime").append("svg").attr("width", width)
+          .datum(testDataWithColorPerTime).call(chart);  
+      }
+
+      function timelineRelativeTime() {
+        //This solution is for relative time is from
+        //http://stackoverflow.com/questions/11286872/how-do-i-make-a-custom-axis-formatter-for-hours-minutes-in-d3-js
+        var chart = d3.timeline()
+          .relativeTime()
+          .tickFormat({
+            format: function(d) { return d3.time.format("%H:%M")(d) },
+            tickTime: d3.time.minutes,
+            tickInterval: 30,
+            tickSize: 15,
+          });
+
+        var svg = d3.select("#timelineRelativeTime").append("svg").attr("width", width)
+          .datum(testDataRelative).call(chart);
+          console.log(testDataRelative);
+      }
+
+      function timelineAxisTop() {
+        var chart = d3.timeline().showAxisTop().stack();
+
+        var svg = d3.select("#timelineAxisTop").append("svg").attr("width", width)
+            .datum(testData).call(chart);
+      }
+
+      function timelineBgndTick() {
+        var chart = d3.timeline().stack().showTimeAxisTick().background('grey');
+
+        var svg = d3.select("#timelineBgndTick").append("svg").attr("width", width)
+            .datum(testData).call(chart);
+      }
+
+      function timelineBgnd() {
+        var chart = d3.timeline().stack().background('grey');
+
+        var svg = d3.select("#timelineBgnd").append("svg").attr("width", width)
+            .datum(testData).call(chart);
+      }
+
+   function timelineComplex() {
+        var chart = d3.timeline();
+     chart.stack();
+     chart.showTimeAxisTick();
+//     chart.showAxisTop();
+//     chart.showToday();
+//     chart.itemHeight(50);
+     chart.margin({left: 250, right: 0, top: 20, bottom: 0});
+     chart.itemMargin(0);
+     chart.labelMargin(25);
+
+     var backgroundColor = "#FCFCFD";
+     var altBackgroundColor = "red";
+     chart.background(function (datum, i) {
+       var odd = (i % 2) === 0;
+       return odd ? altBackgroundColor : backgroundColor;
+     });
+     chart.fullLengthBackgrounds();
+        var svg = d3.select("#timelineComplex").append("svg").attr("width", width)
+            .datum(labelTestData).call(chart);
+      }
+
+      timelineRect();
+      timelineRectNoAxis();
+      timelineCircle();
+      timelineRectAndCircle();
+      timelineHover();
+      timelineStackedIcons();
+      timelineLabelColor();
+      timelineRotatedTicks();
+      timelineRectColors();
+      timelineRectColorsPerTime();
+      timelineRelativeTime();
+      timelineAxisTop();
+      timelineBgndTick();
+      timelineBgnd();
+      timelineComplex();
+
+
   }
 
-timelineRectAndCircle();
+  // Toggle children on click.
+  function click(d) {
+    if (d.children) {
+      d._children = d.children;
+      d.children = null;
+    } else {
+      d.children = d._children;
+      d._children = null;
+    }
+    update(d);
+  }
+
+});
